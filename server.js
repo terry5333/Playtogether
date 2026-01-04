b197a578b3a8ca18b776cd8475c0cef4bd66e809
terry5333 <terrylie0215@gmail.com>
2026-01-04 20:03:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,30 +7,50 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 讓 Express 讀取前端網頁檔案
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
-    console.log('玩家連線：', socket.id);
+// 儲存所有房間的狀態
+const rooms = {};
 
-    // 加入房間
-    socket.on('join_room', (roomId) => {
+io.on('connection', (socket) => {
+    socket.on('join_room', (data) => {
+        const { roomId, maxPlayers, winLines } = data;
         socket.join(roomId);
-        console.log(`玩家 ${socket.id} 加入房間: ${roomId}`);
+
+        // 如果房間不存在，建立新房間，第一個進來的是房主
+        if (!rooms[roomId]) {
+            rooms[roomId] = {
+                admin: socket.id,
+                maxPlayers: parseInt(maxPlayers) || 2,
+                winLines: parseInt(winLines) || 5,
+                players: []
+            };
+        }
+
+        rooms[roomId].players.push(socket.id);
+        
+        // 告訴大家目前房間狀態
+        io.to(roomId).emit('room_update', {
+            isAdmin: socket.id === rooms[roomId].admin,
+            currentPlayers: rooms[roomId].players.length,
+            maxPlayers: rooms[roomId].maxPlayers,
+            winLines: rooms[roomId].winLines
+        });
     });
 
-    // 傳遞遊戲動作
     socket.on('game_move', (data) => {
-        // 將動作發送給同一個房間的其他玩家
         socket.to(data.roomId).emit('receive_move', data);
     });
 
-    socket.on('disconnect', () => {
-        console.log('玩家離線');
+    socket.on('disconnecting', () => {
+        for (const roomId of socket.rooms) {
+            if (rooms[roomId]) {
+                rooms[roomId].players = rooms[roomId].players.filter(id => id !== socket.id);
+                if (rooms[roomId].players.length === 0) delete rooms[roomId];
+            }
+        }
     });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`伺服器已啟動：http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
