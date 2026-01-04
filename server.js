d73d7027facc62b493667b29a818337617fb6bdc
terry5333 <terrylie0215@gmail.com>
2026-01-04 21:16:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
-const spyWords = [["蘋果", "水梨"], ["電腦", "筆電"], ["跑步", "競走"], ["泡麵", "拉麵"]];
-const drawWords = ["皮卡丘", "珍珠奶茶", "長頸鹿", "自由女神", "海綿寶寶", "口罩", "鋼琴"];
 
 io.on('connection', (socket) => {
     socket.on('join_room', (data) => {
-        const { roomId, username, gameType, maxPlayers } = data;
+        const { roomId, username, gameType } = data;
         const rId = roomId.trim();
         socket.join(rId);
         socket.username = username;
@@ -24,51 +22,56 @@ io.on('connection', (socket) => {
         if (!rooms[rId]) {
             rooms[rId] = {
                 gameType, host: socket.id, players: [],
-                gameStarted: false, turnIndex: 0, currentWord: ""
+                gameStarted: false, turnIndex: 0, currentWord: "", currentPainter: null
             };
         }
-        const room = rooms[rId];
-        room.players.push({ id: socket.id, name: username, isAlive: true });
-        io.to(rId).emit('room_update', room);
+        rooms[rId].players.push({ id: socket.id, name: username });
+        io.to(rId).emit('room_update', rooms[rId]);
     });
 
+    // 房主啟動遊戲，通知第一位玩家出題
     socket.on('start_game', (roomId) => {
         const room = rooms[roomId];
         if (!room || room.host !== socket.id) return;
         room.gameStarted = true;
+        sendPickerNotification(roomId);
+    });
 
-        if (room.gameType === 'draw') {
-            room.currentWord = drawWords[Math.floor(Math.random() * drawWords.length)];
-            const painter = room.players[room.turnIndex];
-            io.to(roomId).emit('draw_start', { 
-                painterId: painter.id, 
-                painterName: painter.name,
+    // 通知當前輪到的玩家「設定題目」
+    function sendPickerNotification(roomId) {
+        const room = rooms[roomId];
+        const picker = room.players[room.turnIndex];
+        room.currentPainter = picker.id;
+        io.to(roomId).emit('waiting_for_word', { pickerName: picker.name, pickerId: picker.id });
+    }
+
+    // 玩家提交自己出的題目
+    socket.on('set_word', (data) => {
+        const room = rooms[data.roomId];
+        if (room && socket.id === room.currentPainter) {
+            room.currentWord = data.word;
+            io.to(data.roomId).emit('draw_start', { 
+                painterId: room.currentPainter, 
+                painterName: socket.username,
                 word: room.currentWord 
-            });
-        } else if (room.gameType === 'spy') {
-            const pair = spyWords[Math.floor(Math.random() * spyWords.length)];
-            const spyIdx = Math.floor(Math.random() * room.players.length);
-            room.players.forEach((p, idx) => {
-                io.to(p.id).emit('receive_spy_word', { word: (idx === spyIdx ? pair[1] : pair[0]) });
             });
         }
     });
 
-    // 你畫我猜：畫布座標同步
-    socket.on('drawing', (data) => {
-        // 包含 x, y, lastX, lastY, color, isDrawing
-        socket.to(data.roomId).emit('render_drawing', data);
-    });
+    socket.on('drawing', (data) => socket.to(data.roomId).emit('render_drawing', data));
+    socket.on('clear_canvas', (roomId) => io.to(roomId).emit('do_clear_canvas'));
 
-    socket.on('clear_canvas', (roomId) => {
-        io.to(roomId).emit('do_clear_canvas');
-    });
-
-    // 猜題檢測
     socket.on('send_guess', (data) => {
         const room = rooms[data.roomId];
         if (room && data.msg === room.currentWord) {
-            io.to(data.roomId).emit('game_over', { winner: socket.username, msg: `答案就是 [${room.currentWord}]！` });
+            io.to(data.roomId).emit('round_over', { winner: socket.username, word: room.currentWord });
+            
+            // 輪向下一位
+            room.turnIndex = (room.turnIndex + 1) % room.players.length;
+            setTimeout(() => {
+                io.to(data.roomId).emit('do_clear_canvas');
+                sendPickerNotification(data.roomId);
+            }, 3000);
         } else {
             io.to(data.roomId).emit('chat_msg', { name: socket.username, msg: data.msg });
         }
