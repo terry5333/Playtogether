3551266094a48c438e73bf32f824578f39f5c6fd
terry5333 <terrylie0215@gmail.com>
2026-01-04 22:34:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
-const spyWords = [{n:"泡麵", s:"快煮麵"}, {n:"珍奶", s:"奶茶"}, {n:"西瓜", s:"木瓜"}];
 
 io.on('connection', (socket) => {
     socket.on('join_room', (data) => {
@@ -19,7 +18,7 @@ io.on('connection', (socket) => {
         socket.username = username;
         socket.currentRoom = roomId;
         if (!rooms[roomId]) {
-            rooms[roomId] = { gameType, host: socket.id, players: [], gameStarted: false, turnIdx: 0, currentAnswer: "" };
+            rooms[roomId] = { gameType, host: socket.id, players: [], gameStarted: false, turnIdx: 0 };
         }
         rooms[roomId].players.push({ id: socket.id, name: username });
         io.to(roomId).emit('room_update', rooms[roomId]);
@@ -31,44 +30,42 @@ io.on('connection', (socket) => {
         room.gameStarted = true;
         room.turnIdx = 0;
 
-        if (room.gameType === 'bingo') {
-            io.to(data.roomId).emit('game_begin', { 
-                gameType: 'bingo', 
-                winLines: data.winLines,
-                turnId: room.players[0].id, 
-                turnName: room.players[0].name 
+        // 如果是你畫我猜，立即指定畫家
+        if (room.gameType === 'draw') {
+            const painter = room.players[room.turnIdx];
+            io.to(data.roomId).emit('new_draw_round', { 
+                painterId: painter.id, 
+                painterName: painter.name 
             });
-        } else if (room.gameType === 'draw') {
-            sendNewDrawRound(data.roomId);
-        } else if (room.gameType === 'spy') {
-            const pair = spyWords[Math.floor(Math.random() * spyWords.length)];
-            const spyIdx = Math.floor(Math.random() * room.players.length);
-            room.players.forEach((p, i) => {
-                io.to(p.id).emit('receive_spy_word', { word: (i === spyIdx ? pair.s : pair.n), role: (i === spyIdx ? '臥底' : '平民') });
-            });
-            io.to(data.roomId).emit('game_begin', { gameType: 'spy' });
         }
+        io.to(data.roomId).emit('game_begin', { gameType: room.gameType });
     });
 
-    function sendNewDrawRound(roomId) {
-        const room = rooms[roomId];
-        const painter = room.players[room.turnIdx];
-        io.to(roomId).emit('new_draw_round', { painterId: painter.id, painterName: painter.name });
-    }
-
-    socket.on('bingo_click', (data) => {
+    socket.on('set_word', (data) => {
         const room = rooms[data.roomId];
-        if (room && room.players[room.turnIdx].id === socket.id) {
-            io.to(data.roomId).emit('bingo_sync', data.num);
-            room.turnIdx = (room.turnIdx + 1) % room.players.length;
-            io.to(data.roomId).emit('next_turn', { turnId: room.players[room.turnIdx].id, turnName: room.players[room.turnIdx].name });
+        if (room) {
+            room.currentAnswer = data.word;
+            io.to(data.roomId).emit('chat_msg', { name: "系統", msg: "題目已出好，大家開猜！" });
         }
     });
 
     socket.on('drawing', (data) => socket.to(data.roomId).emit('render_drawing', data));
-    socket.on('set_word', (data) => { if(rooms[data.roomId]) rooms[data.roomId].currentAnswer = data.word; });
-    socket.on('bingo_win', (data) => io.to(data.roomId).emit('round_over', { winner: data.name }));
-    socket.on('send_chat', (data) => io.to(data.roomId).emit('chat_msg', { name: socket.username, msg: data.msg }));
+    socket.on('clear_canvas', (rId) => io.to(rId).emit('do_clear'));
+
+    socket.on('send_chat', (data) => {
+        const room = rooms[data.roomId];
+        if (room && room.gameType === 'draw' && room.currentAnswer && data.msg === room.currentAnswer) {
+            io.to(data.roomId).emit('round_over', { winner: socket.username });
+            // 自動下一輪
+            room.turnIdx = (room.turnIdx + 1) % room.players.length;
+            const nextPainter = room.players[room.turnIdx];
+            setTimeout(() => {
+                io.to(data.roomId).emit('new_draw_round', { painterId: nextPainter.id, painterName: nextPainter.name });
+            }, 3000);
+        } else {
+            io.to(data.roomId).emit('chat_msg', { name: socket.username, msg: data.msg });
+        }
+    });
 });
 
 server.listen(3000, '0.0.0.0');
