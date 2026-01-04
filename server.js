282c66e14f3e75c8d3e9bb9ac54a71b6b66d5baa
terry5333 <terrylie0215@gmail.com>
2026-01-04 20:13:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,48 +9,74 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 儲存所有房間的狀態
 const rooms = {};
 
 io.on('connection', (socket) => {
     socket.on('join_room', (data) => {
-        const { roomId, maxPlayers, winLines } = data;
+        const { roomId, username, maxPlayers, winLines } = data;
         socket.join(roomId);
+        socket.username = username;
+        socket.currentRoom = roomId;
 
-        // 如果房間不存在，建立新房間，第一個進來的是房主
         if (!rooms[roomId]) {
             rooms[roomId] = {
-                admin: socket.id,
+                host: socket.id,
                 maxPlayers: parseInt(maxPlayers) || 2,
                 winLines: parseInt(winLines) || 5,
-                players: []
+                players: [],
+                gameStarted: false,
+                isFinished: false
             };
         }
 
-        rooms[roomId].players.push(socket.id);
+        const room = rooms[roomId];
+        if (room.gameStarted) {
+            socket.emit('error_msg', '遊戲已在進行中，無法加入');
+            return;
+        }
+
+        room.players.push({ id: socket.id, name: username });
         
-        // 告訴大家目前房間狀態
+        // 更新房間資訊給所有人
         io.to(roomId).emit('room_update', {
-            isAdmin: socket.id === rooms[roomId].admin,
-            currentPlayers: rooms[roomId].players.length,
-            maxPlayers: rooms[roomId].maxPlayers,
-            winLines: rooms[roomId].winLines
+            host: room.host,
+            players: room.players,
+            maxPlayers: room.maxPlayers,
+            winLines: room.winLines,
+            gameStarted: room.gameStarted
         });
     });
 
     socket.on('game_move', (data) => {
-        socket.to(data.roomId).emit('receive_move', data);
+        if (rooms[data.roomId] && !rooms[data.roomId].isFinished) {
+            socket.to(data.roomId).emit('receive_move', {
+                number: data.number,
+                senderName: socket.username
+            });
+        }
     });
 
-    socket.on('disconnecting', () => {
-        for (const roomId of socket.rooms) {
-            if (rooms[roomId]) {
-                rooms[roomId].players = rooms[roomId].players.filter(id => id !== socket.id);
-                if (rooms[roomId].players.length === 0) delete rooms[roomId];
+    // 處理獲勝
+    socket.on('player_win', (data) => {
+        const room = rooms[data.roomId];
+        if (room && !room.isFinished) {
+            room.isFinished = true;
+            io.to(data.roomId).emit('game_over', { winner: socket.username });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const roomId = socket.currentRoom;
+        if (rooms[roomId]) {
+            rooms[roomId].players = rooms[roomId].players.filter(p => p.id !== socket.id);
+            if (rooms[roomId].players.length === 0) {
+                delete rooms[roomId];
+            } else {
+                io.to(roomId).emit('room_update', { players: rooms[roomId].players });
             }
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
