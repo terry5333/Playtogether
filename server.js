c2f825e50f14c0881b34d3491f7c5ba536402200
terry5333 <terrylie0215@gmail.com>
2026-01-05 19:36:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
         socket.roomId = roomId;
 
         if (!rooms[roomId]) {
-            rooms[roomId] = { host: socket.id, players: [], gameStarted: false, currentWord: "" };
+            rooms[roomId] = { host: socket.id, players: [], gameStarted: false, gameType: "", settings: {} };
         }
         rooms[roomId].players.push({ id: socket.id, name: username });
         io.to(roomId).emit('room_update', rooms[roomId]);
@@ -29,32 +29,36 @@ io.on('connection', (socket) => {
     socket.on('start_game', (data) => {
         const room = rooms[data.roomId];
         if (!room || room.host !== socket.id) return;
-        
         room.gameStarted = true;
-        if (data.gameType === 'draw') {
-            const words = ["蘋果", "珍珠奶茶", "派大星", "蜘蛛人", "馬里奧"];
-            room.currentWord = words[Math.floor(Math.random() * words.length)];
-            // 隨機選一個畫家
-            const drawer = room.players[Math.floor(Math.random() * room.players.length)];
-            io.to(data.roomId).emit('game_begin', { gameType: 'draw', turnId: drawer.id });
-            io.to(drawer.id).emit('your_word', { word: room.currentWord });
-        } else {
-            io.to(data.roomId).emit('game_begin', { gameType: data.gameType, settings: data.settings });
+        room.gameType = data.gameType;
+        room.settings = data.settings;
+
+        if (data.gameType === 'spy') {
+            // 臥底邏輯：分配詞彙
+            const pairs = [["香蕉", "芭樂"], ["鋼琴", "小提琴"], ["電腦", "平板"]];
+            const pair = pairs[Math.floor(Math.random() * pairs.length)];
+            const spyIdx = Math.floor(Math.random() * room.players.length);
+            room.players.forEach((p, i) => {
+                io.to(p.id).emit('spy_setup', { word: i === spyIdx ? pair[1] : pair[0] });
+            });
+            // 啟動倒數
+            let timeLeft = parseInt(data.settings.spyTime || 60);
+            const timer = setInterval(() => {
+                timeLeft--;
+                io.to(data.roomId).emit('timer_sync', timeLeft);
+                if (timeLeft <= 0) {
+                    clearInterval(timer);
+                    io.to(data.roomId).emit('spy_force_vote');
+                }
+            }, 1000);
         }
+        io.to(data.roomId).emit('game_begin', { gameType: data.gameType, hostId: room.host });
     });
 
-    // 你畫我猜：猜題邏輯
-    socket.on('submit_guess', (data) => {
-        const room = rooms[data.roomId];
-        if (!room || data.guess !== room.currentWord) {
-            io.to(data.roomId).emit('chat_msg', { user: data.username, text: data.guess });
-        } else {
-            io.to(data.roomId).emit('draw_success', { winner: data.username, word: room.currentWord });
-        }
-    });
-
+    socket.on('bingo_click', (data) => io.to(data.roomId).emit('bingo_sync', data.num));
     socket.on('drawing', (data) => socket.to(data.roomId).emit('drawing', data));
-
+    socket.on('submit_guess', (data) => io.to(data.roomId).emit('chat_msg', data));
+    
     socket.on('disconnect', () => {
         if (socket.roomId && rooms[socket.roomId]) {
             rooms[socket.roomId].players = rooms[socket.roomId].players.filter(p => p.id !== socket.id);
