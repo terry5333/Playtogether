969e08648c2d666ec343ba22dc9eb8fad6feddf9
terry5333 <terrylie0215@gmail.com>
2026-01-05 22:09:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 let rooms = {};
-const ADMIN_KEY = "1010215"; // 已更新密鑰
+const ADMIN_KEY = "1010215";
 
 io.on('connection', (socket) => {
     socket.on('create_room', () => {
         const roomId = Math.floor(1000 + Math.random() * 9000).toString();
-        rooms[roomId] = { 
-            host: socket.id, players: [], gameStarted: false, 
-            pickedNumbers: [], currentTurnIdx: 0, scores: {} 
-        };
+        rooms[roomId] = { host: socket.id, players: [], currentTurnIdx: 0, currentWord: "" };
         socket.emit('room_created', { roomId });
     });
 
@@ -29,20 +26,20 @@ io.on('connection', (socket) => {
         socket.roomId = roomId; socket.username = username;
         if (!rooms[roomId].players.find(p => p.id === socket.id)) {
             rooms[roomId].players.push({ id: socket.id, name: username, alive: true });
-            rooms[roomId].scores[username] = 0;
         }
         io.to(roomId).emit('room_update', { roomId, players: rooms[roomId].players, hostId: rooms[roomId].host });
     });
 
-    // 遊戲啟動分配
     socket.on('start_game_with_config', (data) => {
         const room = rooms[data.roomId];
         if (!room || room.host !== socket.id) return;
-        room.config = data.config;
         room.gameType = data.gameType;
 
-        if (data.gameType === 'spy') {
-            const pair = [["蘋果", "水梨"], ["鋼琴", "風琴"]][Math.floor(Math.random()*2)];
+        if (data.gameType === 'draw') {
+            room.currentTurnIdx = 0;
+            sendDrawTurn(data.roomId);
+        } else if (data.gameType === 'spy') {
+            const pair = [["原子筆", "鋼筆"], ["烤肉", "火鍋"]][Math.floor(Math.random()*2)];
             const spyIdx = Math.floor(Math.random() * room.players.length);
             room.players.forEach((p, idx) => {
                 io.to(p.id).emit('game_begin', { gameType: 'spy', word: (idx === spyIdx ? pair[1] : pair[0]), isSpy: (idx === spyIdx), config: data.config });
@@ -52,33 +49,37 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 賓果輪流選號
-    socket.on('bingo_start_picking', (data) => {
-        const room = rooms[data.roomId];
-        room.currentTurnIdx = 0;
-        room.pickedNumbers = [];
-        sendBingoTurn(data.roomId);
+    // 你話我猜專用
+    function sendDrawTurn(roomId) {
+        const room = rooms[roomId];
+        const drawer = room.players[room.currentTurnIdx];
+        io.to(roomId).emit('draw_new_turn', { drawerId: drawer.id, drawerName: drawer.name });
+    }
+
+    socket.on('draw_submit_word', (data) => {
+        rooms[data.roomId].currentWord = data.word;
+        io.to(data.roomId).emit('draw_guessing_stage', { drawerName: socket.username });
     });
 
-    socket.on('bingo_pick_number', (data) => {
+    socket.on('draw_guess', (data) => {
         const room = rooms[data.roomId];
-        const num = parseInt(data.number);
-        if (!room.pickedNumbers.includes(num)) {
-            room.pickedNumbers.push(num);
-            io.to(data.roomId).emit('bingo_number_announced', { number: num, pickerName: socket.username });
+        if (data.guess === room.currentWord) {
+            io.to(data.roomId).emit('draw_correct', { winner: socket.username, word: room.currentWord });
             room.currentTurnIdx = (room.currentTurnIdx + 1) % room.players.length;
-            sendBingoTurn(data.roomId);
+            sendDrawTurn(data.roomId);
         }
     });
 
-    function sendBingoTurn(roomId) {
-        const room = rooms[roomId];
-        const p = room.players[room.currentTurnIdx];
-        io.to(roomId).emit('bingo_next_turn', { activePlayerId: p.id, activePlayerName: p.name });
+    // 賓果開號邏輯
+    socket.on('bingo_start_picking', d => { rooms[d.roomId].currentTurnIdx = 0; sendBingoTurn(d.roomId); });
+    socket.on('bingo_pick_number', d => {
+        io.to(d.roomId).emit('bingo_number_announced', { number: d.number, pickerName: socket.username });
+        rooms[d.roomId].currentTurnIdx = (rooms[d.roomId].currentTurnIdx + 1) % rooms[d.roomId].players.length;
+        sendBingoTurn(d.roomId);
+    });
+    function sendBingoTurn(rid) { 
+        const p = rooms[rid].players[rooms[rid].currentTurnIdx];
+        io.to(rid).emit('bingo_next_turn', { activePlayerId: p.id, activePlayerName: p.name });
     }
-
-    // 管理員 API
-    socket.on('admin_close_room', d => { if(d.key === ADMIN_KEY) io.to(d.roomId).emit('admin_msg', '房解散'); });
 });
-
 server.listen(3000);
