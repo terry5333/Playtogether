a60f8c50f7b4ca40dbd0d5270d8476b09f85ac38
terry5333 <terrylie0215@gmail.com>
2026-01-05 19:45:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,50 +12,68 @@ app.use(express.static(path.join(__dirname, 'public')));
 let rooms = {};
 
 io.on('connection', (socket) => {
+    // 創建房間
+    socket.on('create_room', (data) => {
+        const roomId = Math.floor(1000 + Math.random() * 9000).toString(); // 生成 4 位數房號
+        rooms[roomId] = { host: socket.id, players: [], gameStarted: false, settings: {} };
+        socket.emit('room_created', { roomId });
+    });
+
+    // 加入房間
     socket.on('join_room', (data) => {
         const { roomId, username } = data;
+        if (!rooms[roomId]) return socket.emit('error_msg', '房間不存在');
+        
         socket.join(roomId);
-        if (!rooms[roomId]) {
-            rooms[roomId] = { host: socket.id, players: [], gameStarted: false, settings: {} };
-        }
+        socket.roomId = roomId;
+        socket.username = username;
+        
         rooms[roomId].players.push({ id: socket.id, name: username });
         io.to(roomId).emit('room_update', rooms[roomId]);
     });
 
-    // 痛點 3 & 4：房長按下開始時，帶入連線數或回合數
+    // 開始遊戲
     socket.on('start_game', (data) => {
         const room = rooms[data.roomId];
         if (!room || room.host !== socket.id) return;
         
         room.gameStarted = true;
-        room.settings = data.settings; // 儲存房長的設定
-        
-        // 如果是誰是臥底，啟動倒數
+        room.settings = data.settings;
+
         if (data.gameType === 'spy') {
-            let timeLeft = parseInt(data.settings.spyTime || 60);
+            const time = parseInt(data.settings.spyTime) || 60;
+            let timeLeft = time;
             const timer = setInterval(() => {
                 timeLeft--;
                 io.to(data.roomId).emit('timer_sync', timeLeft);
                 if (timeLeft <= 0) {
                     clearInterval(timer);
-                    io.to(data.roomId).emit('spy_force_vote'); // 時間到強制投票
+                    io.to(data.roomId).emit('spy_force_vote');
                 }
             }, 1000);
+            room.timer = timer;
         }
-        
+
         io.to(data.roomId).emit('game_begin', { 
             gameType: data.gameType, 
             settings: data.settings,
-            turnId: room.players[0].id 
+            players: room.players 
         });
     });
 
-    socket.on('bingo_click', (data) => {
-        io.to(data.roomId).emit('bingo_sync', data.num);
-    });
+    socket.on('bingo_click', (data) => io.to(data.roomId).emit('bingo_sync', data.num));
+    socket.on('drawing', (data) => socket.to(data.roomId).emit('drawing', data));
 
     socket.on('disconnect', () => {
-        // ... 斷線邏輯 ...
+        if (socket.roomId && rooms[socket.roomId]) {
+            rooms[socket.roomId].players = rooms[socket.roomId].players.filter(p => p.id !== socket.id);
+            if (rooms[socket.roomId].players.length === 0) {
+                if(rooms[socket.roomId].timer) clearInterval(rooms[socket.roomId].timer);
+                delete rooms[socket.roomId];
+            } else {
+                io.to(socket.roomId).emit('room_update', rooms[socket.roomId]);
+            }
+        }
     });
 });
 
